4e16e4a29fa6310e5da802df44db5734b09ad888
Rekoyah <bludonut95@gmail.com>
2025-03-26 07:47:27 -0500

    setup welcome prompt

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+document.body.onload = function() {myFunction()};
+function myFunction() {
+    let person = prompt("What is your name, music lover?");
+    if (person != null) {
+        alert("Hello, " + person + ", and welcome to Note Less Traveled.");
+    }
+}
